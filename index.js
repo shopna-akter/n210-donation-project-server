f6bb4c059b3968ff7a7b3b2967c98e6b0274df7c
shopna akter <shopna88akter@gmail.com>
2024-05-13 00:10:11 +0600

    enough for today

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -20,12 +20,56 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        // Send a ping to confirm a successful connection
+        const foodCollection = client.db('foodDB').collection('food')
+        app.get('/foods', async (req, res) => {
+            const cursor = foodCollection.find()
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        app.post('/foods', async (req, res) => {
+            const newFood = req.body
+            console.log(newFood);
+            const result = await foodCollection.insertOne(newFood)
+            res.send(result)
+        })
+        app.put('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const option = { upsert: true }
+            const updatedTour = req.body
+            const tour = {
+                $set: {
+                    short_description: updatedTour.short_description,
+                    totalVisitorsPerYear: updatedTour.totalVisitorsPerYear,
+                    tourists_spot_name: updatedTour.tourists_spot_name,
+                    country_Name: updatedTour.country_Name,
+                    location: updatedTour.location,
+                    seasonality: updatedTour.seasonality,
+                    average_cost: updatedTour.average_cost,
+                    travel_time: updatedTour.travel_time,
+                }
+            }
+            const result = await foodCollection.updateOne(filter, tour, option);
+            res.send(result);
+        });
+        app.delete('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await foodCollection.deleteOne(query);
+            res.send(result)
+        });
+        app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await foodCollection.findOne(query);
+            res.send(result)
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
