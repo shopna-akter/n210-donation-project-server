18c85279326df7a2a32ec1174c2d31cfb10693b8
shopna akter <shopna88akter@gmail.com>
2024-05-14 00:35:58 +0600

    some task left for tommorow

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2yyywnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,11 +25,31 @@ async function run() {
     try {
         await client.connect();
         const foodCollection = client.db('foodDB').collection('food')
-        app.get('/foods', async (req, res) => {
-            const cursor = foodCollection.find()
-            const result = await cursor.toArray();
-            res.send(result)
+
+        // auth related api
+        app.post('/jwt' , async(req ,res)=>{
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET , {expiresIn: '1h'})
+            res
+            .cookie('token' , token, {
+                httpOnly: true ,
+                secure: false,
+            })
+            .send({success: true});
         })
+
+        // direct web operation
+        app.get('/foods', async (req, res) => {
+            try {
+                const result = await foodCollection.find().sort({ Quantity: -1 }).limit(6).toArray();
+                res.json(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Server error' });
+            }
+        });
+        
         app.post('/foods', async (req, res) => {
             const newFood = req.body
             console.log(newFood);
